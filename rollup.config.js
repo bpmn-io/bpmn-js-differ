5ea324a4483594ecd2a3743fbe8c20dfa9e44f13
Nico Rehwaldt <git_nikku@nixis.de>
2018-04-24 00:08:01 +0200

    feat(project): provide pre-built distribution
    + remove ES module hint for now
    + provide CommonJS and ES module distribution

---
+++ rollup.config.js
@@ -0,0 +1,63 @@
+import hypothetical from 'rollup-plugin-hypothetical';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import uglify from 'rollup-plugin-uglify';
+
+import pkg from './package.json';
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: 'lib/index.js',
+    output: {
+      name: 'BpmnJsDiffer',
+      file: 'dist/bpmn-js-differ.umd.js',
+      format: 'umd'
+    },
+    plugins: pgl()
+  },
+  {
+    input: 'lib/index.js',
+    output: {
+      name: 'BpmnJsDiffer',
+      file: 'dist/bpmn-js-differ.umd.min.js',
+      format: 'umd'
+    },
+    plugins: pgl([
+      uglify()
+    ])
+  },
+  {
+    input: 'lib/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: pgl()
+  }
+];
+
+
+function pgl(plugins = []) {
+
+  return [
+    hypothetical({
+      allowFallthrough: true,
+      files: {
+        'chalk': `
+          export default null;
+        `,
+        'diff-match-patch': `
+          export default null;
+        `,
+        'jsondiffpatch/formatters/index': `
+          export default {};
+        `,
+        'jsondiffpatch/formatters/console': `
+          export default null;
+        `
+      }
+    }),
+    nodeResolve(),
+    ...plugins
+  ];
+}
