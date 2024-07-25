48702e41bcc686140eb94aa20bf7daf3de3b90d9
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2024-07-25 16:07:20 +0200

    chore: drop UMD distribution

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,9 @@
 import hypothetical from 'rollup-plugin-hypothetical';
 import nodeResolve from 'rollup-plugin-node-resolve';
-import uglify from 'rollup-plugin-uglify';
 
 import pkg from './package.json';
 
 export default [
-
-  // browser-friendly UMD build
-  {
-    input: 'lib/index.js',
-    output: {
-      name: 'BpmnJsDiffer',
-      file: 'dist/bpmn-js-differ.umd.js',
-      format: 'umd'
-    },
-    plugins: pgl()
-  },
-  {
-    input: 'lib/index.js',
-    output: {
-      name: 'BpmnJsDiffer',
-      file: 'dist/bpmn-js-differ.umd.min.js',
-      format: 'umd'
-    },
-    plugins: pgl([
-      uglify()
-    ])
-  },
   {
     input: 'lib/index.js',
     output: [
